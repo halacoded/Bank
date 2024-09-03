c82ace7c1848729178140850a4eb1944e36a0aff
hessa <hessa@hessas-MacBook-Pro.local>
2024-09-03 19:37:27 +0300

    routerDone

--- src/index.js
+++ src/index.js
@@ -3,10 +3,50 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import { Login } from "./pages/Login";
+import Home from "./pages/Home";
+import Transactions from "./pages/Transactions";
+import Users from "./pages/Users";
+import Profile from "./pages/Profile";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Profile",
+        element: <Profile />,
+      },
+      {
+        path: "/Transactions",
+        element: <Transactions />,
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+      },
+      {
+        path: "/Users",
+        element: <Users />,
+      },
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -14,6 +54,13 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
